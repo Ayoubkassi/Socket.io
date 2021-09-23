c040297d952f44ef2b15e3785205472bdcc12e2a
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-23 14:26:31 +0100

    Implement websocket servere based on http server

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const http = reqUIRE('http');
+const http = require('http');
+const WebSocketServer = require("websocket").server;
+let connection = null;
 
 const httpserver = http.createServer((req,res) => {
   console.log("we have received a request");
 });
+
+const websocket = new WebSocketServer({
+  "httpServer" : httpserver
+})
+
+websocket.on("request", request => {
+    connection =  request.accept(null , request.origin);
+    connection.on("onopen", e => console.log("Openeed!!!"))
+    connection.on("onclose", e => console.log("Clooosed!!!"))
+    connection.on("onmesssage" , message => {
+      console.log(`Message Received : ${message}`);
+    })
+})
 
 httpserver.listen(8080 , () => console.log(`My server is listening on port 8080`))
